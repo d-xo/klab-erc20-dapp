097f3c66902eabafc25439ff939f9f8e3303ad76
David Terry <me@xwvvvvwx.com>
2019-03-17 22:49:58 +0100

    add erc20

---
+++ src/erc20.sol
@@ -0,0 +1,59 @@
+// Copyright (C) 2017, 2018 dbrock, rain
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+pragma solidity ^0.4.24;
+
+contract ERC20 {
+    string  public name;
+    string  public symbol;
+    uint8   public decimals = 18;
+    uint256 public totalSupply;
+
+    mapping (address => uint)                       public  balanceOf;
+    mapping (address => mapping (address => uint))  public  allowance;
+
+    event Approval(address indexed src, address indexed guy, uint wad);
+    event Transfer(address indexed src, address indexed dst, uint wad);
+
+    constructor(string symbol_, string name_) public {
+        symbol = symbol_;
+        name = name_;
+    }
+
+    function approve(address guy, uint wad) public returns (bool) {
+        allowance[msg.sender][guy] = wad;
+        emit Approval(msg.sender, guy, wad);
+        return true;
+    }
+
+    function transfer(address dst, uint wad) public returns (bool) {
+        return transferFrom(msg.sender, dst, wad);
+    }
+
+    function transferFrom(address src, address dst, uint wad)
+        public returns (bool)
+    {
+        require(balanceOf[src] >= wad);
+        if (src != msg.sender && allowance[src][msg.sender] != uint(-1)) {
+            require(allowance[src][msg.sender] >= wad);
+            allowance[src][msg.sender] -= wad;
+        }
+        balanceOf[src] -= wad;
+        balanceOf[dst] += wad;
+        emit Transfer(src, dst, wad);
+        return true;
+    }
+}
+
